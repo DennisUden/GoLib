baa3174e2edafa5ce8480eb13c6f387350ad3cd1
Dennis Uden <info@udenit.de>
2024-03-28 09:49:15 +0100

    added roundup function

--- GoLib.go
+++ GoLib.go
@@ -4,13 +4,12 @@ import (
 	"math"
 )
 
-
 func Round(f float64, d float64) float64 {
 	var h float64 = math.Pow(10,d)
 	return math.Round(f*h)/h
 }
 
 // have to add "to decimal places" functionality
-func RoundUp(f) {
+func RoundUp(f float64) float64 {
 	return math.Ceil(f)	
 }
